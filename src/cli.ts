6ba94b01cf9b45eaf4b565ef176a226185fd6979
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-04-06 13:25:44 +0800

    replace paper-title impl from bash to node.js

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,26 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
+import * as clipboardy from 'clipboardy';
 import { convertTitle } from './core';
 
-let data = fs.readFileSync('/dev/stdin').toString();
-console.log(convertTitle(data));
+console.log('Reading from clipboard');
+// let input = fs.readFileSync('/dev/stdin').toString();
+const input = clipboardy.readSync();
+const output = convertTitle(input);
+
+console.log('== Input ==');
+console.log(input);
+console.log('===========');
+console.log('== Output ==');
+console.log(output);
+console.log('============');
+
+clipboardy
+  .write(output)
+  .then(() => {
+    console.log('Saved to clipboard');
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error('Error:', e);
+    process.exit(1);
+  });
