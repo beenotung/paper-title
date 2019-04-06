8360b809ef6b4dfc1c4f64528e327ad2e5f7ff19
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-04-06 13:24:12 +0800

    support question mark

--- src/core.ts
+++ src/core.ts
@@ -2,26 +2,23 @@ import { flatten } from '@beenotung/tslib/array';
 
 export function replaceRepeated<T>(xs: T[], x: T, replace: T[]): T[] {
   for (let i = 0; i < xs.length; i++) {
-    if (xs[i] === x && xs[i + 1] == x) {
-      let start = i;
+    if (xs[i] === x && xs[i + 1] === x) {
+      const start = i;
       let end = i;
-      for (; xs[end + 1] === x; end++) ;
+      /* tslint:disable:semicolon no-empty */
+      /* tslint:disable: */
+      for (; xs[end + 1] === x; end++) {}
+      /* tslint:enable:semicolon no-empty */
       console.log({ start, end, s: xs[start], e: xs[end] });
-      xs = [
-        ...xs.slice(0, start),
-        ...replace,
-        ...xs.splice(end + 1),
-      ];
+      xs = [...xs.slice(0, start), ...replace, ...xs.splice(end + 1)];
     }
   }
   return xs;
 }
 
 export function convertTitle(s: string): string {
-
   /* deduplicate repeated space */
-  s = s
-    .replace(/\n/g, ' ');
+  s = s.replace(/\n/g, ' ');
 
   /* split by hyphen */
   let words = flatten(s.split('-').map(s => [s, '-']));
@@ -32,12 +29,12 @@ export function convertTitle(s: string): string {
   words = words.filter(s => s);
 
   /* fix broken words */
-  let acc: string[] = [];
+  const acc: string[] = [];
   for (let i = 0; i < words.length; i++) {
-    let word = words[i];
-    if (word.length === 1 && word.toLowerCase() !== 'a' && word != '-') {
+    const word = words[i];
+    if (word.length === 1 && word.toLowerCase() !== 'a' && word !== '-') {
       i++;
-      acc.push(word + words[i]);
+      acc.push(word + (words[i] || ''));
     } else {
       acc.push(word);
     }
@@ -46,7 +43,9 @@ export function convertTitle(s: string): string {
 
   /* fix all cap case */
   if (words.every(s => s === s.toUpperCase())) {
-    words = words.map(s => s.substr(0, 1).toUpperCase() + s.substr(1).toLowerCase());
+    words = words.map(
+      s => s.substr(0, 1).toUpperCase() + s.substr(1).toLowerCase(),
+    );
   }
 
   /* fix adjective case */
@@ -64,10 +63,15 @@ export function convertTitle(s: string): string {
     }
   });
 
-
   /* join back to sentence */
-  s = words.join(' ')
+  s = words
+    .join(' ')
+    /* remove duplicated space */
     .replace(/ {2}/g, ' ')
-    .replace(/ - /g, '-');
+    /* remove space between hypend */
+    .replace(/ - /g, '-')
+    /* remove space before question mark */
+    .replace(/ \?/g, '?');
+
   return s;
 }
--- test/index.ts
+++ test/index.ts
@@ -2,8 +2,15 @@ import * as test from 'tape';
 import { convertTitle } from '../src/core';
 
 test('convert case', t => {
-  let s = 'S TRONGER C ONSISTENCY AND S EMANTICS FOR\n' +
-    'L OW -L ATENCY G EO -R EPLICATED S TORAGE';
-  t.equal(convertTitle(s), 'Stronger Consistency and Semantics for Low-Latency Geo-Replicated Storage');
+  t.equal(
+    convertTitle(
+      'S TRONGER C ONSISTENCY AND S EMANTICS FOR\nL OW -L ATENCY G EO -R EPLICATED S TORAGE',
+    ),
+    'Stronger Consistency and Semantics for Low-Latency Geo-Replicated Storage',
+  );
+  t.equal(
+    convertTitle('CAN NEURAL NETWORKS UNDERSTAND LOGICAL ENTAILMENT ?'),
+    'Can Neural Networks Understand Logical Entailment?',
+  );
   t.end();
 });
